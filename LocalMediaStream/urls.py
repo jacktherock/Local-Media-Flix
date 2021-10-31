f16bf3294aae3fa56d1ec43491bfbc4aef971952
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-10-31 22:59:56 +0530

    Update

--- LocalMediaStream/urls.py
+++ LocalMediaStream/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homedia.urls')),
     path('', include('authentication.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
