21c17de86575a35ebb9a6918070bf6c814c1ef69
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-10-31 23:21:26 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(#fmr$rz)7$t6jhjd6@azy@gee!!1st)=_351+7%-spy^pgi(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localmediaflix.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["localmediaflix.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
