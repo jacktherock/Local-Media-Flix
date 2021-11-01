ca262f9fdaecb923e87bc5ff6f2279ac4a70dbb1
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-11-01 17:00:53 +0530

    Update

--- homedia/models.py
+++ homedia/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Media(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
-    thumbnail = models.ImageField(upload_to='thumbnails', null=True, blank=True, default="")
+    thumbnail = models.ImageField(upload_to='thumbnails', default="")
     title = models.CharField(max_length=1000, blank=True, default="")
     video = models.FileField(upload_to='files', default="")
     created_at = models.DateTimeField(auto_now_add=True)
