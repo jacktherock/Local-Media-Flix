4a88b4b59e2de59e75f1034ba78da3c0e814093a
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-11-03 21:34:22 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -157,8 +157,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 """------------------ Email Backend ------------------"""
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'smax9018@gmail.com'
-EMAIL_HOST_PASSWORD = 'maxs079@'
+EMAIL_HOST_USER = ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
