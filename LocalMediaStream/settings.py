73d632efe84228ebad9e6a22dcc4986500010c6a
Abhijeet <sonawaneabhijeet273@gmail.com>
2022-01-01 17:55:39 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR/'media'
 
