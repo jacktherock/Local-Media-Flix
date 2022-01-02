e959237491b5c7320bd8250c8f526fc7bd6a57a7
Abhijeet <sonawaneabhijeet273@gmail.com>
2022-01-02 19:53:31 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -88,22 +88,22 @@ WSGI_APPLICATION = 'LocalMediaStream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # MongoDB Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'LocalMediaFlixDB',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'LocalMediaFlixDB',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
