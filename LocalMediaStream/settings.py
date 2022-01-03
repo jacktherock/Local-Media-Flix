7c70a6826ad31baee6de174ad2c4f7aa00c2a940
Abhijeet <sonawaneabhijeet273@gmail.com>
2022-01-03 22:55:50 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -88,15 +88,11 @@ WSGI_APPLICATION = 'LocalMediaStream.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 """------------------ Databases ------------------"""
-## Mysql database
+# Sqlite Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'localmediaflixdb-django',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 """-----------------------------------------------"""
