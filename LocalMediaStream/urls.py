c60efdecd8b9e20432e6ab53cdd306a6179d4df3
jacktherock <sonawaneabhijeet273@gmail.com>
2023-02-09 11:37:57 +0530

    Update

--- LocalMediaStream/urls.py
+++ LocalMediaStream/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve
+# from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +16,5 @@ urlpatterns = [
     # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
