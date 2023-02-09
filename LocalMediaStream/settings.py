2d3a302013cc502588ba7bb4fb14af4b72960bab
jacktherock <sonawaneabhijeet273@gmail.com>
2023-02-09 12:38:55 +0530

    Update

--- LocalMediaStream/settings.py
+++ LocalMediaStream/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'LocalMediaStream.wsgi.application'
 """------------------ Databases ------------------"""
 # Sqlite Database
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 """-----------------------------------------------"""
 
